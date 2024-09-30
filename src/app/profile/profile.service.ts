4c1b6d955c4e09f0ad7de1fc5eee8e4d09c8f2f1
Dmitriy Snitko <sr480@live.ru>
2024-09-30 21:30:25 +0300

    Изменить интерфейс обновления профиля пользователя

--- src/app/profile/profile.service.ts
+++ src/app/profile/profile.service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { map, Observable, shareReplay, Subject, tap } from 'rxjs';
 import { ProfileService as ProfileApiService } from '../../api/services';
-import { UpdateUser } from './model/user-update.model';
+import { UserUpdate } from './model/user-update.model';
 import { mapUserResponse, User } from './model/user.model';
 import { continueWith } from '../lib/continue-with';
 @Injectable()
@@ -17,10 +17,12 @@ export class ProfileService {
       shareReplay(1)
     );
 
-  update$(request: UpdateUser): Observable<User> {
-    return this.profileApiService.updateProfile$Json({ body: request }).pipe(
-      map(mapUserResponse),
-      tap((user) => this.updatedProfileSubject.next(user))
-    );
+  update$(request: UserUpdate, rowVersion: string): Observable<User> {
+    return this.profileApiService
+      .updateProfile$Json({ body: { ...request, rowVersion } })
+      .pipe(
+        map(mapUserResponse),
+        tap((user) => this.updatedProfileSubject.next(user))
+      );
   }
 }
