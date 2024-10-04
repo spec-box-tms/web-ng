cf0da7673ed79e483503ba800ddb033f713be997
Dmitriy Snitko <sr480@live.ru>
2024-10-04 23:41:42 +0300

    Добавить WYSIWYG

--- src/app/project/pages/functional-requirements/functional-requirement-details/functional-requirement-details.component.ts
+++ src/app/project/pages/functional-requirements/functional-requirement-details/functional-requirement-details.component.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  Injector,
+  INJECTOR,
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TuiTitle } from '@taiga-ui/core';
 import { combineLatest, map, startWith, switchMap } from 'rxjs';
@@ -6,6 +12,7 @@ import { ProjectContext } from '../../../project-context.service';
 import { FeatureService } from '../../../services/feature.service';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { TuiSkeleton } from '@taiga-ui/kit';
+import { TUI_EDITOR_EXTENSIONS } from '@taiga-ui/editor';
 
 @Component({
   selector: 'app-functional-requirement-details',
@@ -14,6 +21,30 @@ import { TuiSkeleton } from '@taiga-ui/kit';
   templateUrl: './functional-requirement-details.component.html',
   styleUrl: './functional-requirement-details.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: TUI_EDITOR_EXTENSIONS,
+      deps: [INJECTOR],
+      useFactory: (injector: Injector) => [
+        import('@tiptap/starter-kit').then(({ StarterKit }) => StarterKit),
+        import('@taiga-ui/editor').then(({ tuiCreateImageEditorExtension }) =>
+          tuiCreateImageEditorExtension({ injector })
+        ),
+        import('@taiga-ui/editor').then(({ TuiMarkdown }) =>
+          TuiMarkdown.configure({
+            html: true, // Allow HTML input/output
+            tightLists: true, // No <p> inside <li> in markdown output
+            tightListClass: 'tight', // Add class to <ul> allowing you to remove <p> margins when tight
+            bulletListMarker: '-', // <li> prefix in markdown output
+            linkify: true, // Create links from "https://..." text
+            breaks: true, // New lines (\n) in markdown input are converted to <br>
+            transformPastedText: true, // Allow to paste markdown text in the editor
+            transformCopiedText: true, // Copied text is transformed to markdown
+          })
+        ),
+      ],
+    },
+  ],
 })
 export class FunctionalRequirementDetailsComponent {
   private readonly featureCode$ = inject(ActivatedRoute).params.pipe(
